2cd18b161a8171d66ec2053fa5d0b65971fa777d
shantanu561993 <shantanu561993@gmail.com>
2014-08-30 07:49:10 +0530

    first commit

---
+++ spoofmac.py
@@ -0,0 +1,21 @@
+import spoofmac
+from netifaces import interfaces
+
+print "You have following interfaces on your system"
+a=interfaces()
+for i in len(a):
+	print str(i+1)+") "+a[i]
+	
+	
+print "Enter interface number you want to spoof MAC of "
+mac_num=int(raw_input())
+if mac_num>len(a):
+	print "You Entered invalid number"
+	print "Exiting"
+else:
+	mac_inr=a[mac_num-1]
+	b=spoofmac.find_interface(mac_inr)
+	print "Type : %s Name : %s CURRENT_MAC : %s " %(b[0],b[1],b[2])
+	
+	
+	
